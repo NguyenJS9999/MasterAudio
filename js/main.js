fa330adbe40f070d31bee02453e32466b0c81ca4
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-07-15 16:29:12 +0700

    Lam do an web tinh hoan thanh trang home dươc phan loc trang products

--- js/main.js
+++ js/main.js
@@ -1,1 +1,82 @@
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
 console.log("File main")
+
+// Phủ định trạng thái
+let showMenu = false;
+
+function hiddenMenu() {
+    console.log("Ản hiện menu con")
+
+    if (showMenu) {
+        $('.menu-infos-header-mobile').hide();
+    } else {
+        $('.menu-infos-header-mobile').show();
+    }
+    showMenu = !showMenu;
+}
+
+// Menu truợt ngang sản phẩm bán chạy
+$(document).ready(function () {
+    $(".banner-carousel").slick({
+        autoplay: true,
+        autoplaySpeed: 50000,
+        width: 1764,
+        centerMode: true,
+        dots: true,
+
+        arrows: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        
+        slidesToScroll: 1,
+
+  
+        responsive: [
+
+            {
+                breakpoint: 1920,
+                settings: {
+                    slidesToShow: 2.949,
+                },
+            },
+
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2.949,
+                },
+            },
+
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2.949,
+                },
+            },
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1.5,
+                },
+            },
+
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+
+        ],
+
+    });
+});
