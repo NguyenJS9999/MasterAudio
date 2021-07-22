9eeceefdafc569c476479812d5b28044184031ac
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-07-22 10:16:45 +0700

    Lam do an web tinh làm  hau het duoc ui tang home, products, detail sang làm cart check out

--- breadcrumb/breadcrumb.js
+++ breadcrumb/breadcrumb.js
@@ -1,3 +1,3 @@
 // import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-console.log("File navigation")
+console.log("File breadcrumb")
--- product-detail/product-detail.js
+++ product-detail/product-detail.js
@@ -1,89 +1,85 @@
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+console.log("Trang product-detail - Chi tiết sản phẩm")
 
-console.log("file main Trang chủ")
+// $(document).ready(function () {
 
-// Phủ định trạng thái
-// let showMenu = false;
+//     $('.slide-same-products').slick({
 
-// function hiddenMenu() {
-//     console.log("Ản hiện menu con")
+//         slidesToShow: 3,
+//         slidesToScroll: 1
+//     });
 
-//     if (showMenu) {
-//         $('.menu-infos-header-mobile').hide();
-//     } else {
-//         $('.menu-infos-header-mobile').show();
-//     }
-//     showMenu = !showMenu;
-// }
+// }); // $(document).ready(function ()
 
-// Menu truợt ngang sản phẩm bán chạy
+
 $(document).ready(function () {
-    $(".banner-carousel").slick({
+    $(".slide-same-products").slick({
+        infinite: true,
         autoplay: true,
         autoplaySpeed: 5000,
-        // width: 1764,
-        // centerMode: true,
+        centerMode: true,
         dots: true,
 
         // arrows: true,
         pauseOnFocus: true,
         pauseOnHover: true,
-        
+
         slidesToScroll: 1,
 
-  
+
         responsive: [
 
             {
                 breakpoint: 1920,
                 settings: {
-                    slidesToShow: 3.1,
+                    slidesToShow: 3,
                 },
             },
 
             {
                 breakpoint: 1400,
                 settings: {
-                    slidesToShow: 3.1,
+                    slidesToShow: 3,
                 },
             },
 
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3.1,
+                    slidesToShow: 3,
                 },
             },
 
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 2.1,
+                    slidesToShow: 2,
                 },
             },
 
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 1.1,
+                    slidesToShow: 2,
                 },
             },
 
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 0.9,
+                    slidesToShow: 1,
                 },
             },
 
             {
                 breakpoint: 320,
                 settings: {
-                    slidesToShow: 0.6,
+                    slidesToShow: 1,
                 },
             },
 
         ],
 
     });
-});
+
+
+}); // $(document).ready(function ()
