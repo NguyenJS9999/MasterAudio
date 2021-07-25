52a77daceddfc8e48677ecd411c3ec7cac2a12ae
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-07-25 15:46:12 +0700

    Lam do an web tinh làm  hau het duoc ui tang home, products, detail, cart, checkout, đang làm blog, sua nav

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,17 @@
 console.log(" Trang cart - giỏ hàng ")
+
+
+function deleteOneItem() {
+    $(".delete-one").on("click", function deleteOneItem() {
+        $(this).parent().css("display", "none");
+        console.log(`Xóa mỗi item này trong giỏ hàng`)
+        alert("Bạn có chắc chắn muốn xóa sản phẩm này?")
+    });
+}
+
+function deleteAllItem() {
+    $(".cart-items").css("display", "none");
+
+    console.log('Xóa tất cả các item trong giỏ hàng')
+    alert("Bạn có chắc chắn muốn xóa tất cả các sản phẩm trong giỏ hàng chứ?")
+}
