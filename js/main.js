009fcea3606519a74c43354ae81ed3f1a01c09af
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-07-26 16:55:55 +0700

    Lam do an web tinh làm het sach cac trang, sua-them hau het ui index

--- js/main.js
+++ js/main.js
@@ -17,73 +17,146 @@ console.log("file main Trang chủ")
 // }
 
 // Menu truợt ngang sản phẩm bán chạy
+// $(document).ready(function () {
+//     $(".banner-carousel").slick({
+//         autoplay: true,
+//         autoplaySpeed: 5000,
+//         // width: 1764,
+//         // centerMode: true,
+//         dots: true,
+
+//         // arrows: true,
+//         pauseOnFocus: true,
+//         pauseOnHover: true,
+        
+//         slidesToScroll: 1,
+
+  
+//         responsive: [
+
+//             {
+//                 breakpoint: 1920,
+//                 settings: {
+//                     slidesToShow: 3.1,
+//                 },
+//             },
+
+//             {
+//                 breakpoint: 1400,
+//                 settings: {
+//                     slidesToShow: 3.1,
+//                 },
+//             },
+
+//             {
+//                 breakpoint: 1200,
+//                 settings: {
+//                     slidesToShow: 3.1,
+//                 },
+//             },
+
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     slidesToShow: 2.1,
+//                 },
+//             },
+
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1.1,
+//                 },
+//             },
+
+//             {
+//                 breakpoint: 576,
+//                 settings: {
+//                     slidesToShow: 0.9,
+//                 },
+//             },
+
+//             {
+//                 breakpoint: 320,
+//                 settings: {
+//                     slidesToShow: 0.6,
+//                 },
+//             },
+
+//         ],
+
+//     });
+// });
+
 $(document).ready(function () {
-    $(".banner-carousel").slick({
+    $(".slide-same-products").slick({
+        infinite: true,
         autoplay: true,
         autoplaySpeed: 5000,
-        // width: 1764,
-        // centerMode: true,
+        centerMode: true,
         dots: true,
 
         // arrows: true,
         pauseOnFocus: true,
         pauseOnHover: true,
-        
+
         slidesToScroll: 1,
 
-  
+
         responsive: [
 
             {
                 breakpoint: 1920,
                 settings: {
-                    slidesToShow: 3.1,
+                    slidesToShow: 3,
                 },
             },
 
             {
                 breakpoint: 1400,
                 settings: {
-                    slidesToShow: 3.1,
+                    slidesToShow: 3,
                 },
             },
 
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3.1,
+                    slidesToShow: 3,
                 },
             },
 
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 2.1,
+                    slidesToShow: 2,
                 },
             },
 
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 1.1,
+                    slidesToShow: 2,
                 },
             },
 
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 0.9,
+                    slidesToShow: 1,
                 },
             },
 
             {
                 breakpoint: 320,
                 settings: {
-                    slidesToShow: 0.6,
+                    slidesToShow: 1,
                 },
             },
 
         ],
 
     });
-});
+
+
+}); // $(document).ready(function ()
