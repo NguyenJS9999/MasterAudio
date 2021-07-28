9821318c26d9a657ea1d8f066edb5654afe260f1
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-07-28 18:51:39 +0700

    Làm het sach cac trang, sua-them hau het ui chung them mau ink

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -16,3 +16,41 @@ function hiddenMenu() {
     showMenu = !showMenu;
 }
 
+let showLogin = false;
+
+$('.login-register-btn').click(function () {
+    console.log("Hiện biểu mẫu đăng nhập")
+
+    if (showLogin) {
+        $('.login-register-container').hide();
+    } else {
+        $('.login-register-container').show();
+
+    }
+    showLogin = !showLogin;
+})
+
+$(".login-btn ").click(function () {
+    console.log(' Tiến hành đăng nhập - đóng form modal')
+    $('.login-register-container').css('display', 'none');
+});
+
+
+$(".register-btn ").click(function () {
+    console.log(' Tiến hành đăng kí - đóng form modal')
+    $('.login-register-container').css('display', 'none');
+});
+
+// $(".login-register-container").click(function () {
+//     console.log(' click ra ngoài form - đóng form modal')
+//     $('.login-register-container').hide();
+// });
+
+// Đóng modal khi click vào vùng ngoài khối Modal
+$(window).on('click', function (e) {
+    if ($(e.target).is('.login-register-form')) {
+        modal.hide();
+        // css div phủ về trong suốt
+        $('.login-register-container').css("display", "none")
+    }
+});
