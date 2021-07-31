7826dcbf7700d7f5a3d4d06778419fccac257af3
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-07-31 13:06:30 +0700

    Làm het sach cac trang, sua-them icon like tren nav, them trang liên he, sua mau ui va layout

--- js/main.js
+++ js/main.js
@@ -21,77 +21,70 @@
 // Function sẵn sàng - tự động chạy
 $(function () {
 
-// $(".famous-banner-slider").slick({
-//   autoplay: true,
-//   autoplaySpeed: 8000,
-//   speed: 1500,
-//   slidesToShow: 4,
-//   arrows: false,
-//   dots: false,
-//   fade: false,
-//   pauseOnFocus: false,
-//   pauseOnHover: false,
-// });
+  // $(".famous-banner-slider").slick({
+  //   autoplay: true,
+  //   autoplaySpeed: 8000,
+  //   speed: 1500,
+  //   slidesToShow: 4,
+  //   arrows: false,
+  //   dots: false,
+  //   fade: false,
+  //   pauseOnFocus: false,
+  //   pauseOnHover: false,
+  // });
 
-$(".famous-banner-slider").slick({
-  autoplay: true,
-  autoplaySpeed: 5500,
-  speed: 1200,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
-  pauseOnFocus: false,
-  pauseOnHover: false,
-  prevArrow:
-    '<button class="prev-arrow" aria-label="Previous" type="button"><span class="iconify prev-arrow" data-icon="fluent:chevron-left-28-regular" data-inline="false"></span></button>',
-  nextArrow:
-    '<button class="next-arrow" aria-label="Next" type="button"><span class="iconify next-arrow" data-icon="fluent:chevron-right-28-regular" data-inline="false"></span></button>',
+  $(".famous-banner-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 5500,
+    speed: 1200,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    prevArrow: '<button class="prev-arrow" aria-label="Previous" type="button"><span class="iconify prev-arrow" data-icon="fluent:chevron-left-28-regular" data-inline="false"></span></button>',
+    nextArrow: '<button class="next-arrow" aria-label="Next" type="button"><span class="iconify next-arrow" data-icon="fluent:chevron-right-28-regular" data-inline="false"></span></button>',
 
-  responsive: [
-    {
-      breakpoint: 1400,
-      settings: {
-        arrows: false,
-        dots: true,
+    responsive: [{
+        breakpoint: 1400,
+        settings: {},
       },
-    },
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 4,
-        arrows: false,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
 
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
       },
-    },
 
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 1,
-        arrows: false,
-        dots: true,
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
       },
-    },
-  ],
-});
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+
+        },
+      },
+    ],
+  });
 
 
-// Product list
-$(".slide-same-products").slick({
+  // Product list
+  $(".slide-same-products").slick({
     autoplay: true,
     autoplaySpeed: 5500,
     speed: 1250,
@@ -101,13 +94,10 @@ $(".slide-same-products").slick({
     dots: true,
     pauseOnFocus: false,
     pauseOnHover: false,
-    prevArrow:
-      '<button class="prev-arrow" aria-label="Previous" type="button"><span class="iconify prev-arrow" data-icon="fluent:chevron-left-28-regular" data-inline="false"></span></button>',
-    nextArrow:
-      '<button class="next-arrow" aria-label="Next" type="button"><span class="iconify next-arrow" data-icon="fluent:chevron-right-28-regular" data-inline="false"></span></button>',
+    prevArrow: '<button class="prev-arrow" aria-label="Previous" type="button"><span class="iconify prev-arrow" data-icon="fluent:chevron-left-28-regular" data-inline="false"></span></button>',
+    nextArrow: '<button class="next-arrow" aria-label="Next" type="button"><span class="iconify next-arrow" data-icon="fluent:chevron-right-28-regular" data-inline="false"></span></button>',
 
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1400,
         settings: {
           arrows: false,
